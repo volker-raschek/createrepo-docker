9e37f72c0a8b0114d38a2dfd30912b3f7a62b1e7
Markus Pesch <markus.pesch@cryptic.systems>
2022-05-03 15:56:04 +0200

    fix: extract repo dir

--- createrepo.sh
+++ createrepo.sh
@@ -8,9 +8,12 @@ CREATEREPO_IMAGE_VERSION=0.17.2
 CUSTOM_UID=$(getent passwd ${USER} | cut -d ':' -f 3)
 CUSTOM_GID=$(getent passwd ${USER} | cut -d ':' -f 4)
 
+# Extract last element of passed arguments
+REPO_DIR=${@: -1}
+
 ${CONTAINER_RUNTIME} run \
   --rm \
-  --volume ${PWD}:${PWD} \
-  --workdir ${PWD} \
+  --volume ${REPO_DIR}:${REPO_DIR} \
+  --workdir ${REPO_DIR} \
   --user ${CUSTOM_UID}:${CUSTOM_GID} \
   docker.io/volkerraschek/createrepo:${CREATEREPO_IMAGE_VERSION} ${@}
