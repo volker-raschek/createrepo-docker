c4ac21f206491a84b46b2163036b2ff74a075d69
Markus Pesch <markus.pesch@cryptic.systems>
2024-11-08 14:05:27 +0100

    fix(script): set custom uid/gid via env

--- createrepo.sh
+++ createrepo.sh
@@ -4,17 +4,17 @@ set -e
 
 CONTAINER_RUNTIME=$(which docker)
 
-CREATEREPO_IMAGE_FULLY_QUALIFIED=docker.io/volkerraschek/createrepo:0.17.2
+CREATEREPO_IMAGE_FULLY_QUALIFIED="docker.io/volkerraschek/createrepo:0.17.2"
 
-CUSTOM_UID=$(getent passwd ${USER} | cut -d ':' -f 3)
-CUSTOM_GID=$(getent passwd ${USER} | cut -d ':' -f 4)
+CUSTOM_UID="${CUSTOM_UID:-"$(getent passwd "${USER}" | cut -d ':' -f 3)"}"
+CUSTOM_GID="${CUSTOM_GID:-"$(getent passwd "${USER}" | cut -d ':' -f 4)"}"
 
 # Extract last element of passed arguments
-REPO_DIR=${@: -1}
+REPO_DIR="${*: -1}"
 
 ${CONTAINER_RUNTIME} run \
   --rm \
-  --volume ${REPO_DIR}:${REPO_DIR} \
-  --workdir ${REPO_DIR} \
-  --user ${CUSTOM_UID}:${CUSTOM_GID} \
-  ${CREATEREPO_IMAGE_FULLY_QUALIFIED} ${@}
+  --volume "${REPO_DIR}:${REPO_DIR}" \
+  --workdir "${REPO_DIR}" \
+  --user "${CUSTOM_UID}:${CUSTOM_GID}" \
+  "${CREATEREPO_IMAGE_FULLY_QUALIFIED}" "${@}"
